fb9c9a55d891dbf6e7fab7078f016b6f40b3e258
Laurence Hernandez <enzohernandez65@gmail.com>
2025-07-20 15:12:30 +0800

    test api

--- frontend/api/uploadfiles.js
+++ frontend/api/uploadfiles.js
@@ -1,10 +1,6 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://open-slug-nearby.ngrok-free.app/uploadfiles', {
+  const response = await fetch('https://open-slug-nearby.ngrok-free.app/uploadfiles/', {
     method: req.method,
-    headers: {
-      ...req.headers,
-      host: 'open-slug-nearby.ngrok-free.app'
-    },
     body: req.method !== 'GET' ? req.body : null,
   });
 
